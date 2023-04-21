c00cfb2897841b5cc4203a666198549bdaa33971
Marc Verdoorn <100924139+marcverdoorn@users.noreply.github.com>
2023-04-21 21:13:53 +0200

    Add files via upload

---
+++ icrypt/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace icrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.encryptbutton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.keybox = new System.Windows.Forms.TextBox();
+            this.decryptbutton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(317, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SELECT FOLDER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // encryptbutton
+            // 
+            this.encryptbutton.Location = new System.Drawing.Point(317, 76);
+            this.encryptbutton.Name = "encryptbutton";
+            this.encryptbutton.Size = new System.Drawing.Size(158, 23);
+            this.encryptbutton.TabIndex = 1;
+            this.encryptbutton.Text = "ENCRYPT";
+            this.encryptbutton.UseVisualStyleBackColor = true;
+            this.encryptbutton.Click += new System.EventHandler(this.encryptbutton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AccessibleName = "";
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(52, 187);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "custom key";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select folder to en/decrypt";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(52, 220);
+            this.textBox1.MaxLength = 32;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Encryption key:";
+            this.label2.Visible = false;
+            // 
+            // keybox
+            // 
+            this.keybox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keybox.Location = new System.Drawing.Point(52, 132);
+            this.keybox.MaxLength = 32;
+            this.keybox.Name = "keybox";
+            this.keybox.Size = new System.Drawing.Size(247, 26);
+            this.keybox.TabIndex = 5;
+            this.keybox.Visible = false;
+            this.keybox.TextChanged += new System.EventHandler(this.keybox_TextChanged);
+            // 
+            // decryptbutton
+            // 
+            this.decryptbutton.Location = new System.Drawing.Point(317, 105);
+            this.decryptbutton.Name = "decryptbutton";
+            this.decryptbutton.Size = new System.Drawing.Size(158, 23);
+            this.decryptbutton.TabIndex = 6;
+            this.decryptbutton.Text = "DECRYPT";
+            this.decryptbutton.UseVisualStyleBackColor = true;
+            this.decryptbutton.Click += new System.EventHandler(this.decryptbutton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(335, 200);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(130, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 298);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.decryptbutton);
+            this.Controls.Add(this.keybox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.encryptbutton);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button encryptbutton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox keybox;
+        private System.Windows.Forms.Button decryptbutton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ icrypt/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace icrypt
+{
+    public partial class Form1 : Form
+    {
+        public string path=null;
+        public bool customkey = false;
+        public int pushed_decr = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            customkey = checkBox1.Checked;
+            textBox1.Visible = customkey;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+                label1.Text = path;
+            }
+        }
+
+        private void encryptbutton_Click(object sender, EventArgs e)
+        {
+
+            if (path != null)
+            {
+                const string message = "Are you sure you want to encrypt this folder?";
+                const string caption = "Encrypt Folder";
+                var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if(result == DialogResult.Yes)
+                {
+                    string key;
+                    if (!customkey)
+                    {
+                        key = generateKey(32);
+                        keybox.Text = key;
+                        keybox.Visible = true;
+                        keybox.ReadOnly = true;
+                        label2.Visible = true;
+                        encrypt(path, key);
+                    }
+                    else
+                    {
+                        key = textBox1.Text;
+                        if (key.Length != 32)
+                        {
+                            const string message2 = "KEY wrong lenth (32 char needed)";
+                            const string caption2 = "Encrypt Folder";
+                            MessageBox.Show(message2, caption2, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            encrypt(path, key);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                const string message = "NO FOLDER SELECTED";
+                const string caption = "Encrypt Folder";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+        }
+
+        private void decryptbutton_Click(object sender, EventArgs e)
+        {
+            if(pushed_decr == 0)
+            {
+                pushed_decr++;
+                keybox.Visible = true;
+                keybox.ReadOnly = false;
+                label2.Visible = true;
+                
+            }else if (pushed_decr == 1)
+            {
+                string key = keybox.Text;
+                if (key.Length != 32)
+                {
+                    const string message = "KEY wrong lenth (32 char needed)";
+                    const string caption = "Decrypt Folder";
+                    MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (path != null)
+                {
+                    const string message = "Are you sure you want to decrypt this folder?";
+                    const string caption = "Decrypt Folder";
+                    var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        decrypt(path, key);
+                        pushed_decr = 0;
+                    }
+
+                }
+                else
+                {
+                    const string message = "NO FOLDER SELECTED";
+                    const string caption = "Decrypt Folder";
+                    MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+
+        public void encrypt(string path, string key)
+        {
+            string[] files = Directory.GetFiles(path);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = files.Length;
+            progressBar1.Value = 0;
+            progressBar1.Step = 1;
+            progressBar1.Visible = true;
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+                string ext = file.Split('.').Last();
+                string dest = file.Split('.')[0] + "encr." + ext;
+                EncryptFile(file, dest, key);
+                progressBar1.PerformStep();
+                //File.Delete(file);
+            }
+            progressBar1.Visible = false;
+        }
+
+        public void decrypt(string path, string key)
+        {
+            string[] files = Directory.GetFiles(path);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = files.Length;
+            progressBar1.Value = 0;
+            progressBar1.Step = 1;
+            progressBar1.Visible = true;
+            foreach (string file in files)
+            {
+                string ext = file.Split('.').Last();
+                string dest = file.Split('.')[0] + "dcr." + ext;
+
+                DecryptFile(file, dest, key);
+                progressBar1.PerformStep();
+                //File.Delete(file);
+            }
+            progressBar1.Visible = false;
+        }
+
+        public static string generateKey(int n)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+            for (int i = 0; i < n; i++)
+            {
+                ch = chars[random.Next(chars.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+        public static void EncryptFile(string inputFile, string outputFile, string key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = new byte[16];
+
+                using (FileStream fsInput = new FileStream(inputFile, FileMode.Open))
+                {
+                    using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create))
+                    {
+                        using (CryptoStream cs = new CryptoStream(fsOutput, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+
+                            while ((bytesRead = fsInput.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                cs.Write(buffer, 0, bytesRead);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void DecryptFile(string inputFile, string outputFile, string key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = new byte[16];
+
+                using (FileStream fsInput = new FileStream(inputFile, FileMode.Open))
+                {
+                    using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create))
+                    {
+                        using (CryptoStream cs = new CryptoStream(fsInput, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                        {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+
+                            while ((bytesRead = cs.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                fsOutput.Write(buffer, 0, bytesRead);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void keybox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
